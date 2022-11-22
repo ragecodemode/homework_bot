39b853668e734f9e032f21bde7ea3c65dd1ee6f3
nikitaloskutov <nikitaloskutov18@gmail.com>
2022-11-22 22:52:46 +0300

    API

--- homework.py
+++ homework.py
@@ -8,6 +8,20 @@ import requests
 from dotenv import load_dotenv
 
 
+load_dotenv()
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename='error.log',
+    format=(
+        '%(asctime)s, %(levelname)s, %(message)s, %(name)s, %(funcName)s'
+    )
+)
+logger = logging.getLogger(__name__)
+logger.addHandler(
+    logging.StreamHandler()
+)
+
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
@@ -25,7 +39,7 @@ HOMEWORK_STATUSES = {
 
 def send_message(bot, message: str) -> None:
     """отправляет сообщение в Telegram чат."""
-    logger.info('Отправка сообщения в телеграмм чат')
+    logging.info('Отправка сообщения в телеграмм чат')
     try:
         bot.send_message(
             chat_id=TELEGRAM_CHAT_ID,
@@ -34,8 +48,7 @@ def send_message(bot, message: str) -> None:
     except Exception as error:
         logger.error(f'Сообщение {message} об ошибки: {error}')
     else:
-        logger.info(f'Собщение {message} было отправлено')
-
+        logging.info(f'Собщение {message} было отправлено')
 
 def get_api_answer(current_timestamp: int) -> int:
     """Делает запрос к единственному эндпоинту API-сервиса."""
@@ -60,39 +73,46 @@ def get_api_answer(current_timestamp: int) -> int:
 
 def check_response(response):
     """Проверяет ответ API на корректность."""
-    if not isinstance(response['homeworks'], list):
+    if not isinstance(response, dict):
+        raise TypeError('Ответ API отличен от словаря')
+    if 'current_date' and 'homeworks' not in response:
+        message = 'В ответе отсутствуют необходимые ключи'
+        logger.error(message)
+        raise ValueError(message)
+    if not isinstance(response["homeworks"], list):
         message = (
             f'Неккоректное значение в ответе домашней работы {type(response)}'
             f'ожидали: {list(response)}'
         )
-        logging.info(message)
+        logger.error(message)
         raise TypeError(message)
-    if 'homeworks' not in response:
-        message = f'Не найден верный ключ homeworks {response["homeworks"]}'
-        logging.error(message)
-        raise ValueError(message)
     homework = response.get('homeworks')
+    if not isinstance(response["homeworks"], list):
+        message = (
+            f'Неккоректное значение в ответе домашней работы {type(response)}'
+            f'ожидали: {list(response)}'
+        )
+        logger.error(message)
+        raise TypeError(message)
     return homework
 
 
 def parse_status(homework):
     """Извлекает из информации о домашней работе статус этой работы."""
+    if homework.get('homework_name') is None:
+        message = f'Отсутствует имя домашней работы {homework_name}'
+        logger.error(message)
+        raise KeyError(message)
     homework_name = homework.get('homework_name')
     homework_status = homework.get('status')
-    try:
-        homework_name = homework.get('homework_name')
-    except Exception as error:
-        message = f'Осутсвуют ожидаемые ключи: {error}'
-        logging.error(message)
-        raise KeyError(message)
     if homework_status not in HOMEWORK_STATUSES:
         message = (
             f'Не верный статус домашней работы {homework_status}'
-            'Доступыне: approved, reviewing, rejected'
+            f'Доступыне: approved, reviewing, rejected'
         )
         logging.error(message)
         raise KeyError(message)
-    verdict = HOMEWORK_STATUSES[homework_status]
+    verdict = HOMEWORK_STATUSES.get(homework_status)
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
@@ -120,17 +140,6 @@ def check_tokens():
 
 def main():
     """Основная логика работы бота."""
-    load_dotenv()
-    global logger
-
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format=(
-            '%(asctime)s, %(levelname)s, %(message)s, %(name)s, %(funcName)s'
-        )
-    )
-    logger = logging.getLogger(__name__)
-
     if not check_tokens():
         message = 'Отсутствует обязательная переменная окружения'
         logger.critical(message)
